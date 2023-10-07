13294a00a739fcae706a56c069cb5a6eebc4a79d
Drakkarrr <junrey404@gmail.com>
2023-10-08 05:26:40 +0800

    added dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/toggle-dark-mode';
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <ModeToggle />
     </div>
   );
 }
